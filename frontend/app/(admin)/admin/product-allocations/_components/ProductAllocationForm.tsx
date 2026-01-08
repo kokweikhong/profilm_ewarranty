43a8e384afc53a5ae938272e1836bd33c0c7acb5
unknown <kokweikhong@gmail.com>
2026-01-08 22:28:50 +0800

    updated sort shop by branch code

--- frontend/app/(admin)/admin/product-allocations/_components/ProductAllocationForm.tsx
+++ frontend/app/(admin)/admin/product-allocations/_components/ProductAllocationForm.tsx
@@ -247,11 +247,14 @@ export default function ProductAllocationForm({
                     className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-primary/60 sm:text-sm/6"
                   >
                     <option value="">Select a shop</option>
-                    {shops.map((shop, index) => (
-                      <option key={index} value={shop.id}>
-                        {`${shop.branchCode} - ${shop.shopName}`}
-                      </option>
-                    ))}
+                    {/* sort shops by branchCode */}
+                    {shops
+                      .sort((a, b) => a.branchCode.localeCompare(b.branchCode))
+                      .map((shop, index) => (
+                        <option key={index} value={shop.id}>
+                          {`${shop.branchCode} - ${shop.shopName}`}
+                        </option>
+                      ))}
                   </select>
                   <ChevronDownIcon
                     aria-hidden="true"
